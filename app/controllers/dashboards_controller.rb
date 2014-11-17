42703736ac3cb14bdc8b7e4773bf8a993bafe3d6
Arjun Downs <abdowns@gmail.com>
2014-11-17 17:15:35 -0500

    Added listing creation form to new listing page
    * Also added ability to see all listings on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,5 @@
 class DashboardsController < ApplicationController
   def show
-
+    @listings = Listing.all
   end
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,28 @@
 class ListingsController < ApplicationController
   def new
+    @listing = Listing.new
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    current_user.listings << @listing
+
+    if @listing.save
+      redirect_to dashboard_path
+    else
+      redirect_to :back
+    end
   end
 
   def edit
   end
 
   def delete
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :location, :housing_type, :price, :available_from, :available_to)
   end
 end
