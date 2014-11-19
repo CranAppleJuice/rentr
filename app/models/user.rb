b531abfcb2c8fb03b7d64c6be3f8f6184d28b350
Arjun Downs <abdowns@gmail.com>
2014-11-19 15:23:58 -0500

    Added booking model to Rentr
    * Created has many relationships between Users and Listings through Bookings
    * Added bookings to routes, nested under listings
    * Bookings controller includes create and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   validates :password_digest, presence: true
 
   has_many :listings
+  has_many :bookings
+  has_many :booked_listings, through: :bookings, source: :listing
 
   def posted?(listing)
     listings.include?(listing)
