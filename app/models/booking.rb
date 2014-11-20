c8cd217156e37b79d9c1d4177a3ae0dade39e9a9
Arjun Downs <abdowns@gmail.com>
2014-11-20 09:34:32 -0500

    Added custom validations to Booking model
    * Validates that booking is valid, falls within the listing's available
      dates, and does not overlap another booking
    * Adds a custom error message in each validation method
    * Booking form now displays error messages when validation fails

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,48 @@ class Booking < ActiveRecord::Base
 
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :start_date_is_in_future,
+    :date_range_is_valid,
+    :dates_are_not_equal,
+    :listing_is_available,
+    :does_not_overlap_other_bookings
+
+  private
+
+  def start_date_is_in_future
+    if start_date < Date.today
+      errors.add(:start_date, "is in the past!")
+    end
+  end
+
+  def date_range_is_valid
+    if start_date > end_date
+      errors.add(:end_date, "occurs before start date")
+    end
+  end
+
+  def dates_are_not_equal
+    if start_date == end_date
+      errors.add(:end_date, "is the same as start date")
+    end
+  end
+
+  def listing_is_available
+    listing = Listing.find(listing_id)
+    if (start_date < listing.available_from || end_date > listing.available_to)
+      errors.add(:listing, "is unavailable for that date range")
+    end
+  end
+
+  def does_not_overlap_other_bookings
+    listing = Listing.find(listing_id)
+    listing.bookings.each do |booking|
+      if (end_date > booking.start_date && end_date < booking.end_date) ||
+        (start_date > booking.start_date && start_date < booking.end_date) ||
+        (start_date < booking.start_date && end_date > booking.end_date)
+        errors.add(:booking, "overlaps another booking")
+        break
+      end
+    end
+  end
 end
